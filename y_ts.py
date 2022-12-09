f0069fd8afa67b00c5ecfa10857421de9095110e
powebe <55152406+powebe@users.noreply.github.com>
2022-12-09 13:28:28 +0800

    fixed _JB['JB-adj'] =...

--- y_ts.py
+++ y_ts.py
@@ -127,7 +127,7 @@ def mktm(_r_y, _r_x, _startobs, _endobs, _nlag):
   _JB = pd.DataFrame(sms.jarque_bera(u), index=rname)
   _JB.columns = ['JB-stat.']
   #_JB['JB-adj'] = [sms.jarque_bera(u)/nobs*dof,scs.chi.cdf(sms.jarque_bera(u)/nobs*dof,2),'','']
-  _JB['JB-adj'] = df.DataFrame((sms.jarque_bera(model.resid)[0]/nobs*dof,scs.chi.cdf(sms.jarque_bera(model.resid)[0]/nobs*dof,2),sms.jarque_bera(u)[2],sms.jarque_bera(u)[3]))
+  _JB['JB-adj'] = df.DataFrame((sms.jarque_bera(model.resid)[0]/nobs*dof,scs.chi.cdf(sms.jarque_bera(model.resid)[0]/nobs*dof,2),sms.jarque_bera(u)[2],sms.jarque_bera(u)[3]),index=rname)
   print('\n')
   print(_JB.round(4))
   print('\n\n')
